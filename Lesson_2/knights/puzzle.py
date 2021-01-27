ab8ed5ed7d10584c10e00fae617da55597f4aa54
Partheepan Shiyamsun <partheepan_s@live.com>
2021-01-27 13:33:28 +0530

    Worked on Knights and Knave Project. Completed using logical connections(And,Or,Not and Biconditonal)

--- Lesson_2/knights/puzzle.py
+++ Lesson_2/knights/puzzle.py
@@ -12,22 +12,50 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # Known logic that a character cannot be assigned two roles and can only be a Knight or a Knave
+    Or(AKnave, AKnight),
+    Not(And(AKnight, AKnave)),
+
+    # Given that A says it's both:
+    Biconditional(AKnight, (And(AKnight, AKnave))),
+    Biconditional(AKnave, Not(And(AKnight, AKnave)))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # Known logic that a character cannot be assigned two roles and can only be a Knight or a Knave
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+
+    # Given that A says both A and B are Knaves:
+    Biconditional(AKnave, Not(And(AKnave, BKnave))),
+    Biconditional(AKnight, (And(AKnave, BKnave)))
 )
+
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # Known logic that a character cannot be assigned two roles and can only be a Knight or a Knave
+    Or(AKnight,AKnave),
+    Or(BKnight, BKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+
+    # Given that A says both A and B are of the same roles:
+    Biconditional(AKnave, Not(Or(And(AKnave,BKnave), And(AKnight, BKnight)))),
+    Biconditional(AKnight, (Or(And(AKnave,BKnave), And(AKnight, BKnight)))),
+
+    # But B says that they are of different roles:
+    Biconditional(BKnave, Not(Or(And(AKnave,BKnight), And(AKnight, BKnave)))),
+    Biconditional(BKnight, Or(And(AKnave,BKnight), And(AKnight, BKnave)))
 )
+
 
 # Puzzle 3
 # A says either "I am a knight." or "I am a knave.", but you don't know which.
@@ -35,7 +63,36 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    # Known logic that a character cannot be assigned two roles and can only be a Knight or a Knave
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+
+    # Given that A could be either a Knight or a Knave:
+    Or(
+        # If A is a Knight  (will result True from truth table):
+        And(Biconditional(AKnave, AKnave), Biconditional(AKnight, Not(AKnave))),
+        # If A is a Knave (will result False from truth table):
+        And(Biconditional(AKnave, AKnight), Biconditional(AKnight, Not(AKnight)))
+    ),
+
+    # B says that A is a Knave:
+    Biconditional(BKnight, And(Biconditional(AKnave, AKnight), Biconditional(AKnight, Not(AKnight)))),
+    Biconditional(BKnave, Not(And(Biconditional(AKnave, AKnight), Biconditional(AKnight, Not(AKnight))))),
+
+    # B also says that C is a Knave:
+    Biconditional(BKnight, CKnave),
+    Biconditional(BKnave, CKnight),
+
+    # C says A is a Knight:
+    Biconditional(CKnight, AKnight),
+    Biconditional(CKnave, AKnave)
+
+
+
 )
 
 
