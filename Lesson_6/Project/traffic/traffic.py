1bb712cf059ab3874b5ea6b864ebe6127419dbfa
Partheepan Shiyamsun <partheepan_s@live.com>
2021-02-23 12:47:52 +0530

    Completed traffic project and added comments.

--- Lesson_6/Project/traffic/traffic.py
+++ Lesson_6/Project/traffic/traffic.py
@@ -15,29 +15,29 @@ TEST_SIZE = 0.4
 
 def main():
 
-    # Check command-line arguments
+    # check command-line arguments
     if len(sys.argv) not in [2, 3]:
         sys.exit("Usage: python traffic.py data_directory [model.h5]")
 
-    # Get image arrays and labels for all image files
+    # get image arrays and labels for all image files
     images, labels = load_data(sys.argv[1])
 
-    # Split data into training and testing sets
+    # split data into training and testing sets
     labels = tf.keras.utils.to_categorical(labels)
     x_train, x_test, y_train, y_test = train_test_split(
         np.array(images), np.array(labels), test_size=TEST_SIZE
     )
 
-    # Get a compiled neural network
+    # get a compiled neural network
     model = get_model()
 
-    # Fit model on training data
+    # fit model on training data
     model.fit(x_train, y_train, epochs=EPOCHS)
 
-    # Evaluate neural network performance
+    # evaluate neural network performance
     model.evaluate(x_test,  y_test, verbose=2)
 
-    # Save model to file
+    # save model to file
     if len(sys.argv) == 3:
         filename = sys.argv[2]
         model.save(filename)
@@ -58,7 +58,20 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    # create two lists to store image data nd its respective category number
+    images = []
+    category = []
+
+    data_dir = os.path.join(data_dir)
+    for folder in os.listdir(data_dir):
+        folder_path = (os.path.join(data_dir, str(folder)))
+        for image_file in os.listdir(folder_path):
+            img = cv2.imread(os.path.join(folder_path, image_file), cv2.IMREAD_COLOR)
+            res = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT), interpolation=cv2.INTER_AREA)
+            images.append(res)
+            category.append(int(folder))
+
+    return tuple((images, category))
 
 
 def get_model():
@@ -67,7 +80,44 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.models.Sequential([
+        # first convolution layer, learning 32 using a 3 x 3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # first Max-pooling layer using a 2 x 2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # second convolution layer, learning 64 using a 3 x 3 kernel
+        tf.keras.layers.Conv2D(
+            64, (3, 3), activation='relu'
+        ),
+
+        # second Max-pooling layer using a 2 x 2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # flatten units
+        tf.keras.layers.Flatten(),
+
+        # add 3 hidden layers with a dropout of 20%
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dropout(0.20),
+
+        # add an output layer for all categories in the sampling directory
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation='softmax')
+
+   ])
+
+    model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return model
 
 
 if __name__ == "__main__":
